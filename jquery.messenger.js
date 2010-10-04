e80abf13c0c72334999b8f51009bebd7a2cb91e6
Yves Van Broekhoven <yves@mrhenry.be>
2010-10-04 12:54:50 +0200

    Made remove function public, attached options to element

--- jquery.messenger.js
+++ jquery.messenger.js
@@ -3,41 +3,63 @@
  */
 (function($){
   
-  var _add, _remove; 
+  var _add, _remove;
+  
   $.fn.messenger = function(action, options){
     opts = options ? $.extend({}, $.fn.messenger.defaults, options) : $.fn.messenger.defaults;
     $this = this;
+    
     this.each(function(){
+      
+      // Attach options to this element
+      this.messenger_options = opts;
+      
+      // Action
       switch(action){
         case 'add':
           _add($(this), opts);
           break;
         default:
-          _remove($(this), opts);
+          $.fn.messenger.remove($(this), opts);
       }
     });
+    
     return this;
   };
   
   _add = function($this, opts){
-    
     var messengerContainer = $(opts.messengerContainer).addClass(opts.messengerClass).html(opts.messengerContent);
     if ($(opts.messengerClass).length <= 0) {
       messengerContainer.hide().appendTo($this).fadeIn(300);
     }
+    if ($(opts.autoRemove)) {
+      setTimeout(function(){
+        $.fn.messenger.remove($this, opts);
+      }, opts.autoRemoveDelay);
+    }
   }
     
-  _remove = function($this, opts){
-    opts = $.fn.messenger.defaults;
+  $.fn.messenger.remove = function($this, opts){
+    
+    // Set options
+    if ($this[0].messenger_options) {
+      opts = opts ? $.extend({}, this.messenger_options, opts) : this.messenger_options;
+    } else {
+      opts = opts ? $.extend({}, $.fn.messenger.defaults, opts) : $.fn.messenger.defaults;
+    };
+    
+    // Remove messenger
     $('.' + opts.messengerClass, $this).fadeOut(300, function(){
       $(this).remove();
-    })
+    });
   }
   
   $.fn.messenger.defaults = {
     messengerContainer:  '<div></div>',
     messengerClass:      'messenger',
-    messengerContent:    'loading'
+    messengerContent:    'loading',
+    autoRemove:          false,
+    autoRemoveDelay:     2000 
   }
   
 })(jQuery);
